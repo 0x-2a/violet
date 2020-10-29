7fb01b90b0f3d67f79a4dbb3c70f45a2e6758cae
y3sh <joshhibschman@gmail.com>
2020-10-29 08:20:33 -0400

    Timing the lidar

--- violet.go
+++ violet.go
@@ -18,8 +18,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	lidar.Reset()
-	time.Sleep(10 * time.Second)
+
+	info, err := lidar.DeviceInfo()
+	log.Print(info)
+
 	status, errcode, err := lidar.Health()
 	if err != nil {
 		log.Fatal(err)
@@ -29,13 +31,37 @@ func main() {
 		log.Fatalf("Lidar status: %v Error Code: %v\n", status, errcode)
 	}
 	// lidar.StartMotor()
-	scanResults, err := lidar.StartScan(10)
+
+	// time.Sleep(10 * time.Second)
+
+	count := 0
+	iter := 1000
+
+	lastTime := time.Now()
+	err = lidar.StartScanFn(func(p *rplidar.RPLidarPoint) {
+		count++
+		if count % iter == 0 {
+			now := time.Now()
+			timeDiff := now.Sub(lastTime).Seconds()
+			lastTime = now
+			fmt.Printf("%s %d %f per second\n", DebugTimestamp(now.Unix()), count,  float32(iter)/float32(timeDiff))
+		}
+
+		// if p.Angle < 1 {
+		// 	fmt.Printf("Quality: %v\tAngle: %.2f\tDistance: %.2f\n", p.Quality, p.Angle, p.Distance)
+		// }
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, p := range scanResults {
-		fmt.Printf("Quality: %v\tAngle: %.2f\tDistance: %.2f\n", p.Quality, p.Angle, p.Distance)
-	}
+	// for _, p := range scanResults {
+	// 	fmt.Printf("Quality: %v\tAngle: %.2f\tDistance: %.2f\n", p.Quality, p.Angle, p.Distance)
+	// }
 
-	// lidar.Disconnect()
+	// lidar.StopMotor()
+	lidar.Disconnect()
+}
+
+func DebugTimestamp(ts int64) string {
+	return fmt.Sprintf("%d (%s)", ts, time.Unix(ts, 0).UTC().Format("2006-01-02 15:04:05 MST"))
 }
